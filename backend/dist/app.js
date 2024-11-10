f4aa48b64d801a75b07f5bf9447f3ee1fd65a1d6
adarsh <adarshjithu10@gmail.com>
2024-11-10 15:07:45 +0530

    last commit

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -12,7 +12,7 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://my-article-eosin.vercel.app"],
     methods: ["PUT", "PATCH", "GET", "POST", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
